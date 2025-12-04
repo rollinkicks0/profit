d7b6b8919e5c770d0394bbb7caebc103936a6902
Cleaning Professionals <account@cleaningprofessionals.com.au>
2025-12-04 21:53:06 +1100

    Update: Remove SHOPIFY_STORE_DOMAIN for multi-store support

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   env: {
     SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
     SHOPIFY_API_SECRET: process.env.SHOPIFY_API_SECRET,
-    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     SESSION_SECRET: process.env.SESSION_SECRET,
   },
