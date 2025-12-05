bda16f828521f078595a147517dc9efc58b65ba3
Cleaning Professionals <account@cleaningprofessionals.com.au>
2025-12-05 15:19:17 +1100

    Add read_inventory scope to fetch product costs

--- lib/shopify.ts
+++ lib/shopify.ts
@@ -4,7 +4,7 @@ import { shopifyApi, LATEST_API_VERSION } from '@shopify/shopify-api';
 export const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY!,
   apiSecretKey: process.env.SHOPIFY_API_SECRET!,
-  scopes: ['read_orders', 'read_locations', 'read_products'],
+  scopes: ['read_orders', 'read_locations', 'read_products', 'read_inventory'],
   hostName: process.env.NEXT_PUBLIC_APP_URL!.replace(/https?:\/\//, ''),
   apiVersion: LATEST_API_VERSION,
   isEmbeddedApp: false,
